73cbf85fd0c37ba50af88738a0ac6fde45122c15
piotrpoczwardowski <piotrpoczwardowski@gmail.com>
2019-04-17 11:46:27 +0200

    respon for subpages

---
+++ subPages.js
@@ -0,0 +1,16 @@
+let backgroundImage = document.querySelector('.background__image img')
+let backgroundText = document.querySelector('.background__blur')
+let background = document.querySelector('.background__image')
+window.onload = function(){
+backgroundText.style.height = backgroundImage.height + 'px'
+}
+
+window.addEventListener("resize", function(event) {
+    if(window.innerWidth > 875){
+        backgroundText.style.height = backgroundImage.height + 'px'
+    } else{
+        background.style.display = 'none'
+    }
+
+
+});
